9465e9f6cb2e0c106dd51cf305baabda9d6039ff
INVISIBLE <goshakovan14@gmail.com>
2020-03-21 16:12:12 +0200

    Your awesome message

---
+++ src/components/App.js
@@ -0,0 +1,117 @@
+import React, { Component } from "react";
+import MovieItem from "./MovieItem";
+import MovieTabs from "./MovieTabs";
+import { API_URL, API_KEY_3 } from "../utils/api";
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            movies: [],
+            moviesWillWatch: [],
+            sort_by: "popularity.desc"
+        };
+    }
+
+    componentDidMount() {
+        this.getMovies();
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log("didUpdate");
+        console.log("prev", prevProps, prevState);
+        console.log("this", this.props, this.state);
+
+        if (prevState.sort_by !== this.state.sort_by) {
+            console.log("call api");
+
+            this.getMovies();
+        }
+    }
+
+    getMovies = () => {
+        fetch(`${API_URL}/discover/movie?api_key=${API_KEY_3}&sort_by=${this.state.sort_by}`)
+            .then(response => {
+                console.log("then", response);
+                return response.json();
+            })
+            .then(data => {
+                this.setState({
+                    movies: data.results
+                });
+            });
+    };
+
+    removeMovie = id => {
+        const { movies } = this.state;
+        this.setState({
+            movies: movies.filter(movie => {
+                return movie.id !== id;
+            })
+        });
+    };
+
+    addWillWatchMovie = movie => {
+        const { moviesWillWatch } = this.state;
+        this.setState({
+            moviesWillWatch: [...moviesWillWatch, movie]
+        });
+    };
+
+    removeWillWatchMovie = id => {
+        const { moviesWillWatch } = this.state;
+        this.setState({
+            moviesWillWatch: moviesWillWatch.filter(movie => {
+                return movie.id !== id;
+            })
+        });
+    };
+
+    updateSortBy = value => {
+        this.setState({
+            sort_by: value
+        });
+    };
+
+    render() {
+        console.log("Data", this.state);
+        const { movies, moviesWillWatch, sort_by } = this.state;
+        const count = moviesWillWatch.length;
+        return (
+            <div className="container">
+                <div className="row mt-4">
+                    <div className="col-9">
+                        <div className="row mb-4">
+                            <div className="col-12">
+                                <MovieTabs
+                                    sort_by={sort_by}
+                                    updateSortBy={this.updateSortBy}
+                                ></MovieTabs>
+                            </div>
+                            {movies.map(movie => {
+                                return (
+                                    <div className="col-6 mb-4" key={movie.id}>
+                                        <MovieItem
+                                            movie={movie}
+                                            removeMovie={this.removeMovie}
+                                            addWillWatchMovie={this.addWillWatchMovie}
+                                            removeWillWatchMovie={this.removeWillWatchMovie}
+                                        ></MovieItem>
+                                    </div>
+                                );
+                            })}
+                        </div>
+                    </div>
+                    <div className="col-3">
+                        <p className="willWatch_title">Will watch: {count} movies</p>
+                        {moviesWillWatch.map(movie => {
+                            return <p className="willWatch_item">{movie.title}</p>;
+                        })}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/utils/api.js
@@ -0,0 +1,6 @@
+export const API_URL = "https://api.themoviedb.org/3";
+
+export const API_KEY_3 = "587069c32d65f89155f78c18cd9a93b4";
+
+export const API_KEY_4 =
+    "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ODcwNjljMzJkNjVmODkxNTVmNzhjMThjZDlhOTNiNCIsInN1YiI6IjVlNzQ5NTI5MzU3YzAwMDAxMTRkNjAzNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.IU5W9I_qNQLwhKOLqMEHr4T1vLrcw4DIlTJicSip768";
